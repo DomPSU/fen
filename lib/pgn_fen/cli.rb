d555766bd680971603ef0c6d15073301fee2d856
Dominic Lupo <DominicLupo5318@gmail.com>
2019-10-24 10:00:38 -0400

    Update input to take multiple fen arguements and then combine into a single fen arguement for display method.

--- lib/pgn_fen/cli.rb
+++ lib/pgn_fen/cli.rb
@@ -4,9 +4,9 @@ require 'pgn_fen'
 module PgnFen
   class CLI < Thor
     desc "display FEN", "Displays fen."
-    def display(fen)
-      display = Display.new(fen).board
-      print(display)
+    def display(*fen)
+      fen = Display.new(fen.join(" "))
+      print(fen.board)
     end
   end
 end
