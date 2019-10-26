870746472ddda331dc3af40077e7d3aea0c835f1
Dominic Lupo <DominicLupo5318@gmail.com>
2019-10-26 06:44:53 -0400

    Add display method for debugging.

--- lib/fen/fen_to_board.rb
+++ lib/fen/fen_to_board.rb
@@ -1,12 +1,13 @@
 require_relative 'chess_constants'
 
-class Fen_To_Board
+class FenToBoard
   attr_accessor :fen
-  attr_reader :piece_array
+  attr_reader :piece_array, :board_array
 
   def initialize(fen)
     @fen = fen
     @piece_array = convert_fen_to_piece_array
+    @board_array = convert_piece_array_to_board_array
   end
 
   def convert_fen_to_piece_array
@@ -22,8 +23,21 @@ class Fen_To_Board
   end
 
   def convert_piece_array_to_board_array
-    board array = [["_"]*8]*8
+    @board_array = [["_"]*8]*8
+  end
+
+  def display
+    display = ""
+    @board_array.each do |rank|
+      display += rank.join
+      display += "\n" 
+    end
+    display
   end
 end
 
 fen = "r1bqk2r/pppp1ppp/2n2n2/2b1p3/2BPP3/2P2N2/PP3PPP/RNBQK2R b KQkq - 0 5"
+
+var = FenToBoard.new(fen)
+#print(var.convert_piece_array_to_board_array)
+print(var.display)
