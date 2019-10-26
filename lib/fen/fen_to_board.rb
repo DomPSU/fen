d8559ebc48033eddb64f21d47951c31ec98fe376
Dominic Lupo <DominicLupo5318@gmail.com>
2019-10-26 06:53:02 -0400

    Change board_array to display_array. Change piece_array to board_array.

--- lib/fen/fen_to_board.rb
+++ lib/fen/fen_to_board.rb
@@ -2,33 +2,33 @@ require_relative 'chess_constants'
 
 class FenToBoard
   attr_accessor :fen
-  attr_reader :piece_array, :board_array
+  attr_reader :board_array, :display_array
 
   def initialize(fen)
     @fen = fen
-    @piece_array = convert_fen_to_piece_array
-    @board_array = convert_piece_array_to_board_array
+    @board_array = convert_fen_to_board_array
+    @display_array = convert_board_array_to_display_array
   end
 
-  def convert_fen_to_piece_array
+  def convert_fen_to_board_array
     fen_array = @fen.split("/")
 
     first_rank_and_details = fen_array.last.split(" ")
     first_rank = first_rank_and_details[0]
 
-    piece_array = fen_array
-    piece_array[-1] = first_rank
+    board_array = fen_array
+    board_array[-1] = first_rank
     
-    @piece_array = piece_array
+    @board_array = board_array
   end
 
-  def convert_piece_array_to_board_array
-    @board_array = [["_"]*8]*8
+  def convert_board_array_to_display_array
+    @display_array = [["_"]*8]*8
   end
 
   def display
     display = ""
-    @board_array.each do |rank|
+    @display_array.each do |rank|
       display += rank.join
       display += "\n" 
     end
@@ -39,5 +39,7 @@ end
 fen = "r1bqk2r/pppp1ppp/2n2n2/2b1p3/2BPP3/2P2N2/PP3PPP/RNBQK2R b KQkq - 0 5"
 
 var = FenToBoard.new(fen)
-#print(var.convert_piece_array_to_board_array)
+#print(var.convert_board_array_to_display_array)
 print(var.display)
+
+#change piece array to board array in display class. Just makes more sense.
