bd278411148651fdc4861449f26c901fe0a72c06
Dominic Lupo <DominicLupo5318@gmail.com>
2019-10-28 14:02:52 -0400

    Update display class and remove repeated code from FenState class. Update display testing as neccesary.

--- test/display_test.rb
+++ test/display_test.rb
@@ -5,12 +5,6 @@ class DisplayTests < Minitest::Test
     @display = Display.new(fen)
   end
 
-  def test_display_fen_returns_fen
-    fen = "r1bqk2r/pppp1ppp/2n2n2/2b1p3/2BPP3/2P2N2/PP3PPP/RNBQK2R b KQkq - 0 5"
-
-    assert_equal fen, @display.fen
-  end
-
   def test_board
     board = @display.file_notation_row
     board += @display.top_row
@@ -34,18 +28,6 @@ class DisplayTests < Minitest::Test
 
     assert_equal board, @display.board
   end
-
-  def test_convert_fen_to_piece_array
-    assert_equal "r1bqk2r", @display.piece_array[0]
-    assert_equal "pppp1ppp", @display.piece_array[1]
-    assert_equal "2n2n2", @display.piece_array[2]
-    assert_equal "2b1p3", @display.piece_array[3]
-    assert_equal "2BPP3", @display.piece_array[4]
-    assert_equal "2P2N2", @display.piece_array[5]
-    assert_equal "PP3PPP", @display.piece_array[6]
-    assert_equal "RNBQK2R", @display.piece_array[7]
-    assert_nil @display.piece_array[8]
-  end 
 
   def test_file_notation_row
     assert_equal "   a  b  c  d  e  f  g  h  \n", @display.file_notation_row
